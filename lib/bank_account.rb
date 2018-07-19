fb97a3a03cb331473f4f6b196d8c083ecc343938
Natasha K <kapr.natasha@gmail.com>
2018-07-19 20:00:02 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+  # @@all_accounts = []
+
+  def initialize(name, balance = 1000, status = 'open')
+    @name = name
+    @balance = balance
+    @status = status
+    # @@all_accounts << self
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @status == "open" && @balance > 0
+  end
+
+def close_account
+  self.status= "closed"
+end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,43 @@
 class Transfer
-  # your code here
+attr_accessor :sender, :receiver, :status, :amount
+
+def initialize(sender, receiver, status="pending", amount)
+  @sender = sender
+  @receiver = receiver
+  @status = status
+  @amount = amount
 end
+
+def valid?
+  sender.valid? && receiver.valid?
+end
+
+def execute_transaction
+  #check if sender has enough funds
+  if @status == "pending"
+
+    if sender.balance >= @amount
+      receiver.deposit(@amount)
+      sender.balance -= @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+
+    end
+
+  else
+    return
+  end
+end
+
+def reverse_transfer
+  if @status == "complete"
+    @status = "reversed"
+    receiver.balance -= @amount
+    sender.deposit(@amount)
+
+  end
+end
+
+end #end transfer class
